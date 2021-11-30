be8b7755d46aa95632430ff53245d00745d86083
vitaly <kazancevvitalijj@gmail.com>
2021-11-30 22:03:13 +0800

    def parse

--- jobparser/jobparser/spiders/hhru.py
+++ jobparser/jobparser/spiders/hhru.py
@@ -1,4 +1,5 @@
 import scrapy
+from scrapy.http import HtmlResponse
 
 
 class HhruSpider(scrapy.Spider):
@@ -8,5 +9,13 @@ class HhruSpider(scrapy.Spider):
         'https://irkutsk.hh.ru/search/vacancy?schedule=remote&search_field=name&search_field=company_name&search_field=description&fromSearchLine=true&text=Python'
     ]
 
-    def parse(self, response):
-        pass
+    def parse(self, response: HtmlResponse):
+        next_page = response.xpath('//a[@data-qa="pager-next"]/@href').get()
+        if next_page:
+            yield response.follow(next_page, callback=self.parse)
+        links = response.xpath('//a[@data-qa="vacancy-serp__vacancy-title"]/@href').getall()
+        for link in links:
+            yield response.follow(link, callback=self.job_parse)
+
+    def job_parse(self, response: HtmlResponse):
+        print()
--- jobparser/jobparser/spiders/superjob.py
+++ jobparser/jobparser/spiders/superjob.py
@@ -1,5 +1,5 @@
 import scrapy
-
+from scrapy.http import HtmlResponse
 
 class SuperjobSpider(scrapy.Spider):
     name = 'superjob'
@@ -8,5 +8,13 @@ class SuperjobSpider(scrapy.Spider):
         'https://www.superjob.ru/vacancy/search/?keywords=Python&remote_work_binary=2&noGeo=1&click_from=facet'
     ]
 
-    def parse(self, response):
-        pass
+    def parse(self, response: HtmlResponse):
+        next_page = response.xpath('//a[contains(@class, "f-test-link-Dalshe")]/@href').get()
+        if next_page:
+            yield response.follow(next_page, callback=self.parse)
+        links = response.xpath('//a[@target="_blank"]/@href').getall()
+        for link in links:
+            yield response.follow(link, callback=self.job_parse())
+
+    def job_parse(self, response: HtmlResponse):
+        print()
