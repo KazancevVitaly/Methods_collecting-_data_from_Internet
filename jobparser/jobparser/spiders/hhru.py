7fcd957732bc0902e89d8239022ae95e536fb0b3
vitaly <kazancevvitalijj@gmail.com>
2021-12-01 09:37:29 +0800

    def job_parse

--- jobparser/jobparser/spiders/hhru.py
+++ jobparser/jobparser/spiders/hhru.py
@@ -16,6 +16,22 @@ class HhruSpider(scrapy.Spider):
         links = response.xpath('//a[@data-qa="vacancy-serp__vacancy-title"]/@href').getall()
         for link in links:
             yield response.follow(link, callback=self.job_parse)
+        """
+        Пересмотрел урок несколько раз 5 и всё равно не могу понять этот момент.
+        Не теряем ли мы здесь первую страницу с вакансиями.
+        Почему мы не делаем так:
+        links = response.xpath('//a[@data-qa="vacancy-serp__vacancy-title"]/@href').getall()
+        for link in links:
+            yield response.follow(link, callback=self.job_parse)
+        next_page = response.xpath('//a[@data-qa="pager-next"]/@href').get()
+        if next_page:
+            yield response.follow(next_page, callback=self.parse)
+        Можно прокомментировать прямо здесь в пулле.
+        """
 
     def job_parse(self, response: HtmlResponse):
         print()
+        name = response.xpath('//h1//text()').get()
+        salary = response.xpath('//div[@class="vacancy-salary"]//text()').getall()
+        job_link = response.url
+
--- jobparser/jobparser/spiders/superjob.py
+++ jobparser/jobparser/spiders/superjob.py
@@ -5,16 +5,22 @@ class SuperjobSpider(scrapy.Spider):
     name = 'superjob'
     allowed_domains = ['superjob.ru']
     start_urls = [
-        'https://www.superjob.ru/vacancy/search/?keywords=Python&remote_work_binary=2&noGeo=1&click_from=facet'
+        'https://www.superjob.ru/vacancy/search/?keywords=python&remote_work_binary=2&noGeo=1&click_from=facet'
     ]
 
     def parse(self, response: HtmlResponse):
+        links = response.xpath('//a[contains(@class, "icMQ_ _6AfZ9")]/@href').getall()
+        for link in links:
+            # link = f'https://www.superjob.ru{link}'
+            yield response.follow(link, callback=self.job_parse)
         next_page = response.xpath('//a[contains(@class, "f-test-link-Dalshe")]/@href').get()
+        # next_page = f'https://www.superjob.ru{next_page}'
         if next_page:
             yield response.follow(next_page, callback=self.parse)
-        links = response.xpath('//a[@target="_blank"]/@href').getall()
-        for link in links:
-            yield response.follow(link, callback=self.job_parse())
 
     def job_parse(self, response: HtmlResponse):
         print()
+        name = response.xpath('//h1/text()').get()
+        salary = response.xpath('//span[@class="_2Wp8I _1e6dO _1XzYb _3Jn4o"]/text()').getall()
+        job_link = response.url
+
